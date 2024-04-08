f081b576a3d5111c92db0474611ec8085a65697d
ng-aman <98579307+ng-aman@users.noreply.github.com>
2024-04-08 14:42:49 +0530

    Revert "added google auth"

--- rag_app_v1.py
+++ rag_app_v1.py
@@ -8,128 +8,98 @@ import streamlit as st
 from langchain_community.chat_message_histories.dynamodb import (
     DynamoDBChatMessageHistory,
 )
-from streamlit_google_auth import login, logout
-
 
 load_dotenv()
 
-secret_config = json.loads(os.environ["SECRET_CONFIG"])
-
-cookie_name = "my_cookie_name"
-cookie_key = "this_is_secret"
-cookie_expiry_days = 30
-app_url = "https://ngenux-rag-aws.streamlit.app"
-color = "blue"  # Or 'white'
-justify_content = "center"
-
-
-st.markdown(
-    "<h1 style='text-align: center;'>Ngenux RAG ChatBot</h1>", unsafe_allow_html=True
+boto3_session = boto3.Session(
+    aws_access_key_id=os.environ["ACCESS_KEY"],
+    aws_secret_access_key=os.environ["SECRET_KEY"],
+    region_name=os.environ["REGION"],
 )
 
-login(
-    secret_config=secret_config,
-    cookie_name=cookie_name,
-    cookie_key=cookie_key,
-    cookie_expiry_days=cookie_expiry_days,
-    app_url=app_url,
-    color=color,
-    justify_content=justify_content,
+table_name = "SessionTable"
+dynamodb_client = boto3.client(
+    "dynamodb",
+    aws_access_key_id=os.environ["ACCESS_KEY"],
+    aws_secret_access_key=os.environ["SECRET_KEY"],
+    region_name=os.environ["REGION"],
 )
+dynamodb = boto3.resource(
+    "dynamodb",
+    aws_access_key_id=os.environ["ACCESS_KEY"],
+    aws_secret_access_key=os.environ["SECRET_KEY"],
+    region_name=os.environ["REGION"],
+)
+table = dynamodb.Table(table_name)
 
 
-if st.session_state["connected"]:
+def send_request_to_api_gateway(json_body):
+    try:
+        # Define the API Gateway endpoint URL
+        api_url = os.environ["API_URL"]
 
-    boto3_session = boto3.Session(
-        aws_access_key_id=os.environ["ACCESS_KEY"],
-        aws_secret_access_key=os.environ["SECRET_KEY"],
-        region_name=os.environ["REGION"],
+        # Make a POST request to the API Gateway endpoint with the JSON body
+        response_from_api = requests.post(api_url, json=json_body)
+        response_data = response_from_api.json()
+        return response_data
+    except Exception as e:
+        # Handle errors
+        print("Error:", e)
+        return {"error": "Internal server error"}
+
+
+def get_sessions(table_name):
+    response = dynamodb_client.scan(TableName=table_name)
+    unique_session_ids = list(set(item["SessionId"]["S"] for item in response["Items"]))
+    return unique_session_ids
+
+
+def get_session_history(session_id, dynamodb_table="SessionTable"):
+    history = DynamoDBChatMessageHistory(
+        table_name=dynamodb_table, session_id=session_id, boto3_session=boto3_session
     )
+    return history
 
-    table_name = "SessionTable"
-    dynamodb_client = boto3.client(
-        "dynamodb",
-        aws_access_key_id=os.environ["ACCESS_KEY"],
-        aws_secret_access_key=os.environ["SECRET_KEY"],
-        region_name=os.environ["REGION"],
-    )
-    dynamodb = boto3.resource(
-        "dynamodb",
-        aws_access_key_id=os.environ["ACCESS_KEY"],
-        aws_secret_access_key=os.environ["SECRET_KEY"],
-        region_name=os.environ["REGION"],
-    )
-    table = dynamodb.Table(table_name)
 
-    def send_request_to_api_gateway(json_body):
-        try:
-            # Define the API Gateway endpoint URL
-            api_url = os.environ["API_URL"]
+with st.sidebar:
+    st.title("RAG ChatBot")
+    st.subheader("With Memory :brain:")
+sessions = get_sessions("SessionTable")
+new_session = st.sidebar.chat_input("New session name")
+seleceted_session = st.sidebar.selectbox("Session Id", sessions, index=None)
 
-            # Make a POST request to the API Gateway endpoint with the JSON body
-            response_from_api = requests.post(api_url, json=json_body)
-            response_data = response_from_api.json()
-            return response_data
-        except Exception as e:
-            # Handle errors
-            print("Error:", e)
-            return {"error": "Internal server error"}
 
-    def get_sessions(table_name):
-        response = dynamodb_client.scan(TableName=table_name)
-        unique_session_ids = list(
-            set(item["SessionId"]["S"] for item in response["Items"])
-        )
-        return unique_session_ids
+def get_chat_history(session):
+    st.sidebar.write(f"current session:  **{session}**")
 
-    def get_session_history(session_id, dynamodb_table="SessionTable"):
-        history = DynamoDBChatMessageHistory(
-            table_name=dynamodb_table,
-            session_id=session_id,
-            boto3_session=boto3_session,
-        )
-        return history
+    if session:
+        history = get_session_history(session_id=session)
 
-    with st.sidebar:
-        st.title("RAG ChatBot")
-        st.subheader("With Memory :brain:")
-    sessions = get_sessions("SessionTable")
-    new_session = st.sidebar.chat_input("New session name")
-    seleceted_session = st.sidebar.selectbox("Session Id", sessions, index=None)
+        if history.messages == []:
+            history.add_ai_message("How may I assist you today?")
 
-    def get_chat_history(session):
-        st.sidebar.write(f"current session:  **{session}**")
+        for msg in history.messages:
+            st.chat_message(msg.type).write(msg.content)
 
-        if session:
-            history = get_session_history(session_id=session)
+    if prompt := st.chat_input():
+        st.chat_message("human").write(prompt)
+        # getting chat response from lambda
+        payload = {"question": prompt, "session_id": session}
+        response = send_request_to_api_gateway(payload)
+        body = response["body"]
+        st.chat_message("ai").write(body)
 
-            if history.messages == []:
-                history.add_ai_message("How may I assist you today?")
 
-            for msg in history.messages:
-                st.chat_message(msg.type).write(msg.content)
+# create session state to store session ids in order to avoid sending none values to get_chat_history
 
-        if prompt := st.chat_input():
-            st.chat_message("human").write(prompt)
-            # getting chat response from lambda
-            payload = {"question": prompt, "session_id": session}
-            response = send_request_to_api_gateway(payload)
-            body = response["body"]
-            st.chat_message("ai").write(body)
+if "session" not in st.session_state:
+    st.session_state["session"] = None
 
-    # create session state to store session ids in order to avoid sending none values to get_chat_history
+if new_session:
+    st.session_state["session"] = new_session
+elif seleceted_session:
+    st.session_state["session"] = seleceted_session
 
-    if "session" not in st.session_state:
-        st.session_state["session"] = None
+# calling get_chat_history
 
-    if new_session:
-        st.session_state["session"] = new_session
-    elif seleceted_session:
-        st.session_state["session"] = seleceted_session
-
-    # calling get_chat_history
-
-    get_chat_history(st.session_state["session"])
-
-    if st.sidebar.button("Log out"):
-        logout(cookie_name)
+get_chat_history(st.session_state["session"])
